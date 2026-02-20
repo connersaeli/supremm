0f54f70220426d697d02b79ef93424dd67755cec
Conner Saeli <connersa@buffalo.edu>
2026-02-20 14:34:11 -0500

    .

--- tests/component/runtests.sh
+++ tests/component/runtests.sh
@@ -3,12 +3,13 @@ set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 outputfile=`mktemp`
+cat $DIR/../../config/config.json
 python3 $DIR/../../src/supremm/supremm_testharness.py -c $DIR/../../config $DIR/data/perfevent > $outputfile
 
 # Check that there are data in the output for all the following
-jq -e .cpuperf.cpiref.avg < $outputfile 
-jq -e .cpuperf.flops.avg < $outputfile 
-jq -e .cpuperf.cpldref.avg < $outputfile 
+jq -e .cpuperf.cpiref.avg < $outputfile
+jq -e .cpuperf.flops.avg < $outputfile
+jq -e .cpuperf.cpldref.avg < $outputfile
 jq -e .uncperf.membw.avg < $outputfile
 jq -e .timeseries.membw < $outputfile
 jq -e .timeseries.simdins < $outputfile
